8a2cb1e12556916f2424c493c8fbc21e2962cfd0
MGulamhussein <mazharali.gulamhussein@hotmail.com>
2018-03-21 10:33:50 +0000

    coding challenge 5

--- javascript/codingChallenge4onwards.js
+++ javascript/codingChallenge4onwards.js
@@ -4,16 +4,27 @@ var person = {
     occupation: "Software Tester"
 };
 
-function ageCheck (){
-    if (person.age >= 20 && person.age <=40){
+function ageCheck() {
+    if (person.age >= 20 && person.age <= 40) {
         console.log("person is between 20 and 40 years old")
-    }
-
-    else {
+    } else {
         console.log("person is not between 20 and 40 years old")
     }
 }
 
 ageCheck();
 
-
+function fizzBuzz() {
+    var numberList = [];
+    for (var i = 0; i < 100; i++) {
+        numberList[i] = i + 1;
+        if (numberList[i] % 3 === 0 && numberList[i] % 5 === 0) {
+            numberList[i] = "FizBuzz";
+        } else if (numberList[i] % 3 === 0) {
+            numberList[i] = "Fizz";
+        } else if (numberList[i] % 5 === 0) {
+            numberList[i] = "Buzz";
+        }
+    }
+    console.log(numberList)
+}
